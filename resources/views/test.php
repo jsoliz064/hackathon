8cdb235ceb5648c01741aa11cd005c6851ef06b2
pericena12@gmail.com <pericena12@gmail.com>
2021-11-07 12:02:52 -0400

    app

--- resources/views/test.php
+++ resources/views/test.php
@@ -13,7 +13,7 @@
 
 
 
-$exif = exif_read_data("IMG_20211106_103303.jpg"); $lon = getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']); $lat = getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']); var_dump($lat, $lon);
+$exif = exif_read_data("ee01bf82-f4cf-40df-8e05-21be38567cd2.jpg"); $lon = getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']); $lat = getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']); var_dump($lat, $lon);
 
 
 function getGps($exifCoord, $hemi) { $degrees = count($exifCoord) > 0 ? gps2Num($exifCoord[0]) : 0; $minutes = count($exifCoord) > 1 ? gps2Num($exifCoord[1]) : 0; $seconds = count($exifCoord) > 2 ? gps2Num($exifCoord[2]) : 0; $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1; return $flip * ($degrees + $minutes / 60 + $seconds / 3600); } function gps2Num($coordPart) { $parts = explode('/', $coordPart); if (count($parts) <= 0) return 0; if (count($parts) == 1) return $parts[0]; return floatval($parts[0]) / floatval($parts[1]); }
